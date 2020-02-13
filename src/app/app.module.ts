5823cb291ed4d9d15c274fea4ec2304553905640
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-02-13 23:46:10 +1100

    home page first draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
