f7184ee2c048941dd3f85fa152c219fbbd166816
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-02-15 13:20:06 +1100

    finish outer background delete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,24 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const a = window.innerHeight;
+    document.getElementById('smallphotobackground').style.background = 'white';
+    document.getElementById('bottomcontact').style.marginTop = String(a - 40 + 'px');
   }
-
+  zheng(){
+    document.body.style.background = 'pink';
+    document.getElementById('smallphotobackground').style.background = 'pink';
+  }
+  zhu(){
+    document.body.style.background = 'cornflowerblue';
+    document.getElementById('smallphotobackground').style.background = 'cornflowerblue';
+  }
+  yi(){
+    document.body.style.background = 'orange';
+    document.getElementById('smallphotobackground').style.background = 'orange';
+  }
+  shi(){
+    document.body.style.background = 'mediumpurple';
+    document.getElementById('smallphotobackground').style.background = 'mediumpurple';
+  }
 }
