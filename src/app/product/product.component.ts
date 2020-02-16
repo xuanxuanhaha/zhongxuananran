7275d6c002d842a42e4a11390cb825c11ae19f07
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-02-16 14:54:33 +1100

    product page first version

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  fourbuttonshow = false;
   constructor() { }
-
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    // if ((window.innerHeight + window.scrollY) <= document.body.offsetHeight) {
+    //   // you're at the bottom of the page
+    //
+    //   document.getElementById('smallphotobackground').style.background = 'pink';
+    // }
+    console.log(window.scrollY);
+    if(window.scrollY > 100){
+      document.getElementById('navbarbackground').style.display = 'none';
+    }else{
+      document.getElementById('navbarbackground').style.display = 'inline';
+    }
+  }
   ngOnInit() {
+    this.fourbuttonshow = false;
+  }
+
+  chooseallproducts(){
+    if(this.fourbuttonshow === false){
+    console.log(this.fourbuttonshow);
+    this.fourbuttonshow = true;
+    }else{
+      this.fourbuttonshow = false;
+    }
   }
 
 }
